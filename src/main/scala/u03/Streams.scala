d8dfe11d215a5bb45ef97311045061235adec7cd
MarcoMoro10 <marco.morelli15@studio.unibo.it>
2025-04-22 16:38:34 +0200

    Stream soluzione esercizio 6

--- src/main/scala/u03/Streams.scala
+++ src/main/scala/u03/Streams.scala
@@ -59,6 +59,7 @@ end Streams
   val str3 = Stream.filter(str2)(x => (x < 3 || x > 20)) // {1,2,21,22,..}
   val str4 = Stream.take(str3)(10) // {1,2,21,22,..,28}
   println(Stream.toList(str4)) // [1,2,21,22,..,28]
+  println(Stream.toList(Stream.takeWhile(str1)(_ < 10)))
 
   lazy val corec: Stream[Int] = Stream.cons(1, corec) // {1,1,1,..}
   println(Stream.toList(Stream.take(corec)(10))) // [1,1,..,1]
